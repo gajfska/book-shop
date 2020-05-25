c9878faab3f912446f50a52963c546182e42af0f
Werka <weronika.gajska@gmail.com>
2020-05-25 20:15:45 +0200

    create book model

---
+++ src/app/booklist/book.model.ts
@@ -0,0 +1,12 @@
+export class Book {
+    public name: string;
+    public author: string;
+    public image: string;
+
+    constructor(name: string, author: string, image: string) {
+        this.name = name;
+        this.author = author;
+        this.image = image;
+    }
+
+}
