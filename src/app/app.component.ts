2e0ae4482153a4d106f842449c1ff340d39e9955
Werka <weronika.gajska@gmail.com>
2020-05-27 22:44:00 +0200

    adding navigation in header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'book-store';
+
+  loadedFeature = 'shopping';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
