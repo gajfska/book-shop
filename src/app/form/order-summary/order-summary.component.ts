5d0b7ed36a33b841a047ab5ac996b3173ae5e531
Werka <weronika.gajska@gmail.com>
2020-06-15 00:23:29 +0200

    adding book from form to order summary

--- src/app/form/order-summary/order-summary.component.ts
+++ src/app/form/order-summary/order-summary.component.ts
@@ -1,6 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Addres, User} from '../form.model';
 import {FormService} from '../form.service';
+import {Book} from '../../shared/book.model';
+import {CartService} from '../../cart/cart.service';
 
 @Component({
     selector: 'app-order-summary',
@@ -10,11 +12,16 @@ import {FormService} from '../form.service';
 export class OrderSummaryComponent implements OnInit{
     user: User;
     addres: Addres;
+    books: Book[];
+    sum: number;
 
-    constructor(private formService: FormService) {}
+    constructor(private formService: FormService,
+                private cartService: CartService) {}
 
     ngOnInit() {
         this.user = this.formService.user;
         this.addres = this.formService.addres;
+        this.books = this.cartService.getBooks();
+        this.sum = this.cartService.count();
     }
 }
